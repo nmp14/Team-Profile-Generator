0f5a5a7678c1873a41ce1473318b9353a9e1d096
Nathan Peek <nmp14fsu@gmail.com>
2021-02-21 22:18:39 -0500

    Add prompt for choices for adding members or finish building team

--- buildTeam.js
+++ buildTeam.js
@@ -1,9 +1,26 @@
+const inquirer = require("inquirer");
 const Team = require("./lib/Team");
 
-const buildTeam = (manager) => {
-    const team = new Team;
+// Create obj for the team
+const team = new Team;
+
+const buildTeam = async (manager) => {
+    // Add manager to team.
     team.addMember(manager);
-    console.log(team);
+
+    // Prompt user if they wish to add more members (and which if so) or finish building.
+    const promptAnswer = await teamBuildChoices();
+
+}
+
+const teamBuildChoices = () => {
+    return inquirer.prompt([
+        {
+            name: "options",
+            type: "list",
+            choices =["Add engineer", "Add intern", "Finish building my team"]
+        }
+    ])
 }
 
 module.exports = buildTeam;
