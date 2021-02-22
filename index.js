3b65ebcff58da37481835fdc42451dc5ab7ecb6a
Nathan Peek <nmp14fsu@gmail.com>
2021-02-21 21:29:46 -0500

    Add init function with functionality to prompt for team manager info. Calls team builder function after

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = require("generateHTML");
+//const generateHTML = require("generateHTML");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const buildTeam = require("./buildTeam");
+
+const init = async () => {
+    const answer = await getManagerInfo();
+    const manager = new Manager(answer.managerName, answer.employeeID, answer.email, answer.number);
+    // Call team builder to finish adding team members and pass manager as argument
+    buildTeam(manager);
+}
+
+const getManagerInfo = () => {
+    return inquirer.prompt([
+        {
+            name: "managerName",
+            message: "Enter name of team manager"
+        },
+        {
+            name: "employeeID",
+            message: "Enter employee ID of team manager"
+        },
+        {
+            name: "email",
+            message: "Enter email"
+        },
+        {
+            name: "number",
+            message: "Enter office number"
+        }
+    ])
+}
+
+init();
