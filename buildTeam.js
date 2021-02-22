b62febc41e7022a8fd43a08751c3c8b5d36046ff
Nathan Peek <nmp14fsu@gmail.com>
2021-02-21 22:43:46 -0500

    Add login to adding engineer to team

--- buildTeam.js
+++ buildTeam.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer");
 const Team = require("./lib/Team");
+const getEngineerInfo = require("./helper_functions/getEngineerInfo");
+const Engineer = require("./lib/Engineer");
 
 // Create obj for the team
 const team = new Team;
@@ -11,6 +13,17 @@ const buildTeam = async (manager) => {
     // Prompt user if they wish to add more members (and which if so) or finish building.
     const promptAnswer = await teamBuildChoices();
 
+    switch (promptAnswer.options) {
+        case "Add engineer":
+            addEngineer();
+            break;
+        case "Add intern":
+            addIntern();
+            break;
+        case "Finish building my team":
+            buildTeam();
+            break;
+    }
 }
 
 const teamBuildChoices = () => {
@@ -18,9 +31,18 @@ const teamBuildChoices = () => {
         {
             name: "options",
             type: "list",
-            choices =["Add engineer", "Add intern", "Finish building my team"]
+            choices: ["Add engineer", "Add intern", "Finish building my team"]
         }
     ])
+}
+
+const addEngineer = async () => {
+    const engineerInfo = await getEngineerInfo();
+    const engineer = new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.github)
+    team.addMember(engineer);
+
+    // Call teambuild choices again after engineer is made and added to team.
+    teamBuildChoices();
 }
 
 module.exports = buildTeam;
